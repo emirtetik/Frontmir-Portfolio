246dee9d4e827d779a65561b83676020bb168d31
Emir Tetik <info@frontmir.com>
2023-07-07 22:57:15 +0300

    Markdown style eklendi responsive hale getirildi

--- components/MarkAbout/MarkdownAbout.js
+++ components/MarkAbout/MarkdownAbout.js
@@ -1,86 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MarkAboutcss from '../../styles/MarkdownAbout.module.css';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import 'highlight.js/styles/monokai.css';
 
 function MarkdownAbout() {
-
   const syntaxHighlighterStyles = {
-    width: '100%',
-    maxWidth: '50rem',
-    height: '40rem',
-    padding: '0px',
     background: 'transparent',
-    color: 'white',
+    color: '#f8f8f2',
   };
 
+  const defaultAbout = `const merhabaBenEmir = () => {
 
-  const defaultAbout = ` const merhabaBenEmir = () => {
+  return (
+    <div className={MarkAboutcss.container}>
+      <div className={MarkAboutcss.row}>
+        <p className={MarkAboutcss.text}>
 
-return (
-  <div className={MarkAboutcss.container}>
-    <div className={MarkAboutcss.row}>
-     <p className={MarkAboutcss.text}>
-      Yaklaşık 1 sene önce hosting şirketinde çalışmaya ve
-      eğitim almaya başladım.Bu işe başladığım zaman sitelerin 
-      ön yüzlerinin nasıl yapıldığı ve front-end developerın 
-      ne olduğuna dair en ufak bir fikrim yoktu. Daha sonra yaptığım
-      işin detaylarına girdikçe benim asıl keyif alabileceğim 
-      mesleğin front-end developer olmak olduğunun farkına vardım.
-      Bilgisayar başında bir şeyler araştırmak, bu araştırmaları 
-      deneyimlemek ve bu deneyimlerle yeni bir şeyler üretmeyi 
-      seviyorum. Bir yandan para kazanırken bir yandan da 
-      sevdiğim işi yapabilecek olduğum için front-end developer 
-      olmaya karar verdim.Bu mesleği yapabilmek için gerekli 
-      olan donanımı birçok ücretsiz kaynaktan araştırma yaparak
-      ve eğitim alarak kazanmaya çalışıyorum. Bu araştırmalara
-      ve eğitimlere günümün yaklaşık 10 saatini ayırarak
-      kendimi geliştiriyorum.Bu yolda çok ilerlediğimi 
-      düşünüyorum ve ilerlemeye devam edeceğim.
-      </p>
+          Yaklaşık 1 sene önce hosting şirketinde çalışmaya ve
+          eğitim almaya başladım.Bu işe başladığım zaman sitelerin 
+          ön yüzlerinin nasıl yapıldığı ve front-end developerın 
+          ne olduğuna dair en ufak bir fikrim yoktu. Daha sonra yaptığım
+          işin detaylarına girdikçe benim asıl keyif alabileceğim 
+          mesleğin front-end developer olmak olduğunun farkına vardım.
+          Bilgisayar başında bir şeyler araştırmak, bu araştırmaları 
+          deneyimlemek ve bu deneyimlerle yeni bir şeyler üretmeyi 
+          seviyorum. Bir yandan para kazanırken bir yandan da 
+          sevdiğim işi yapabilecek olduğum için front-end developer 
+          olmaya karar verdim.Bu mesleği yapabilmek için gerekli 
+          olan donanımı birçok ücretsiz kaynaktan araştırma yaparak
+          ve eğitim alarak kazanmaya çalışıyorum. Bu araştırmalara
+          ve eğitimlere günümün yaklaşık 10 saatini ayırarak
+          kendimi geliştiriyorum.Bu yolda çok ilerlediğimi 
+          düşünüyorum ve ilerlemeye devam edeceğim.
+
+        </p>
       </div>
       <div className={MarkAboutcss.additionalRow}>
-       <p className={MarkAboutcss.text}> 
-       Kocaeli Üniversitesi İngilizce-İktisat bölümü mezunuyum.
-       25 yaşındayım ve kod yazmaktan çok keyif alıyorum.
-       Öğrendiğim çoğu bilgiyi diğer front-end
-       developerların paylaşımları ve yardımları sayesinde öğrendim.
-       Bu yüzden ben de bu yolda öğrendiklerimi ve yaptığım işleri 
-       diğer front-end developer arkadaşlarım için blog
-       sayfamda paylaşıyorum. Web geliştirme alanında sorularınız
-       için sosyal medya hesaplarımdan bana ulaşabilirsiniz.
-       </p>
-         
+        <p className={MarkAboutcss.text}> 
+
+          Kocaeli Üniversitesi İngilizce-İktisat bölümü mezunuyum.
+          25 yaşındayım ve kod yazmaktan çok keyif alıyorum.
+          Öğrendiğim çoğu bilgiyi diğer front-end
+          developerların paylaşımları ve yardımları sayesinde öğrendim.
+          Bu yüzden ben de bu yolda öğrendiklerimi ve yaptığım işleri 
+          diğer front-end developer arkadaşlarım için blog
+          sayfamda paylaşıyorum. Web geliştirme alanında sorularınız
+          için sosyal medya hesaplarımdan bana ulaşabilirsiniz.
+          
+        </p>
       </div>
     </div>
-    )
-  }
-`;
+  )
+}`;
 
+  const syntaxHighlighterRef = useRef(null);
   const [input, setInput] = useState(defaultAbout);
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
   };
 
+  useEffect(() => {
+    hljs.registerLanguage('javascript', javascript);
+  }, []);
 
-  
+  useEffect(() => {
+    if (syntaxHighlighterRef.current) {
+      hljs.highlightBlock(syntaxHighlighterRef.current);
+    }
+  }, [input]);
+
   return (
     <div className={MarkAboutcss.container}>
       <div className={MarkAboutcss.editor}>
         <h2 className={MarkAboutcss.editor_head}>Editor</h2>
         <div className={MarkAboutcss.panel}><p>Vs Code</p></div>
-        <textarea autoFocus className={MarkAboutcss.textarea} value={input} onChange={handleInputChange} />
+        <textarea
+          autoFocus
+          className={MarkAboutcss.textarea}
+          value={input}
+          onChange={handleInputChange}
+        />
       </div>
       <div className={MarkAboutcss.preview}>
         <h2 className={MarkAboutcss.preview_head}>Preview</h2>
         <div className={MarkAboutcss.markdown}>
-          <SyntaxHighlighter language="javascript" style={docco} wrapLongLines={true} customStyle={syntaxHighlighterStyles}>
-        {input}
-          </SyntaxHighlighter>
+          <pre>
+            <code
+              ref={syntaxHighlighterRef}
+              className="javascript hljs"
+              style={syntaxHighlighterStyles}
+            >
+              {input}
+            </code>
+          </pre>
         </div>
       </div>
     </div>
   );
 }
+
 export default MarkdownAbout;
--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,9 @@ export default function Document() {
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Monoton&display=swap" rel="stylesheet"/>        
+
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/monokai.css"  />
+          <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
 </Head>
 
 
